788b4ab5b58fd375d71a5723cab89473de23fef6
Sam Nolan <samnolan555@gmail.com>
2016-11-14 17:21:28 +1100

    Change weights output to be JS Friendly

--- Hex DLC/FileIO.cpp
+++ Hex DLC/FileIO.cpp
@@ -7,7 +7,7 @@ void FileIO::saveWeightsToFile(std::string filename, std::vector<double> vector)
 
 	if (outFile.is_open()) {
 		for (auto element : vector) {
-			outFile << element << std::endl;
+			outFile << element << ",";
 		}
 		outFile.close();
 	}
@@ -20,8 +20,10 @@ std::vector<double> FileIO::readWeightsFromFile(std::string filename) {
 
 	if (inFile.is_open()) {
 		std::string line;
-		while (std::getline(inFile, line, '\n')) {
-			weights.push_back(std::stod(line));
+		while (std::getline(inFile, line, ',')) {
+			if (line.empty()) {
+				weights.push_back(std::stod(line));
+			}
 		}
 	}
 
--- Hex DLC/Source.cpp
+++ Hex DLC/Source.cpp
@@ -7,47 +7,49 @@
 
 #include "FileIO.h"
 
-	int main() {
+int main() {
 
-		Population pop = Population(POP_SIZE);
+	Population pop = Population(POP_SIZE);
 
-		pop.scoreMembers();
+	pop.scoreMembers();
 
-		for (int i = 0; i < POP_SIZE; i++) {
-			std::cout << pop.getMember(i).m_score << ", ";
-		}
-
-		std::cout << "\n";
-
-		pop.sortMembers();
-
-		for (int i = 0; i < POP_SIZE; i++) {
-			std::cout << pop.getMember(i).m_score << ", ";
-		}
-
-		
-
-		//Network Test
-		/*Network net = Network(2, { 2 }, 1);
-		net.setWeights({ 1, 0, 0, 0.5, 1.341597, 0, 101, 7.4, 0 });
-
-		std::cout << "New Network Weights:\n";
-		std::vector<double> weights = net.getWeights();
-		for (auto weight : weights) {
-			std::cout << weight << " ";
-		}
-		std::cout << "\n\n";
-
-		std::cout << "Network output: " << net.getOutput({ 0.5, 0.5 })[0] << "\n";
-		std::cout << "Total Weights: " << net.getNumWeights() << "\n";*/
-
-
-		// Save algorithm of champion
-		//std::vector<double> wieghts = pop.getMember(0).m_network.getWeights();
-		//FileIO::saveWeightsToFile("Champ.txt", wieghts);
-
-
-		std::cout << "\n";
-		std::getchar();
-		return 0;
+	for (int i = 0; i < POP_SIZE; i++) {
+		std::cout << pop.getMember(i).m_score << ", ";
 	}
+
+	std::cout << "\n";
+
+	pop.sortMembers();
+
+	for (int i = 0; i < POP_SIZE; i++) {
+		std::cout << pop.getMember(i).m_score << ", ";
+	}
+
+
+
+	//Network Test
+	/*Network net = Network(2, { 2 }, 1);
+	net.setWeights({ 1, 0, 0, 0.5, 1.341597, 0, 101, 7.4, 0 });
+
+	std::cout << "New Network Weights:\n";
+	std::vector<double> weights = net.getWeights();
+	for (auto weight : weights) {
+		std::cout << weight << " ";
+	}
+	std::cout << "\n\n";
+
+	std::cout << "Network output: " << net.getOutput({ 0.5, 0.5 })[0] << "\n";
+	std::cout << "Total Weights: " << net.getNumWeights() << "\n";*/
+
+
+	// Save Weights of champion
+	//std::vector<double> wieghts = pop.getMember(0).m_network.getWeights();
+	//FileIO::saveWeightsToFile("Champ.txt", wieghts);
+	// Read Weights
+	//std::vector<double> recoveredWeights = FileIO::readWeightsFromFile("Champ.txt");
+
+
+	std::cout << "\n";
+	std::getchar();
+	return 0;
+}

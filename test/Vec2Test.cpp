8c387a662ae7cf1c06fd9f340d8b740a40ee181a
Sam Nolan <samnolan555@gmail.com>
2017-01-23 18:53:07 +1100

    Add Testing Framework
    With example shown for a Vec2 Test

---
+++ test/Vec2Test.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE Vec2
+#include <boost/test/included/unit_test.hpp>
+#include "Vec2.hpp"
+
+BOOST_AUTO_TEST_CASE( initVectorTo0 ){
+	Vec2 vector;
+	BOOST_TEST( vector.x == 0 );
+	BOOST_TEST( vector.y == 0 );
+}
